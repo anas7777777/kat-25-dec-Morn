9df864ccdc153f870be7732df0ba7970609c4f0a
anas7777777 <103210123+anas7777777@users.noreply.github.com>
2022-04-07 23:09:33 +0530

    Update HomePageClass.java

--- src/main/java/pages/HomePageClass.java
+++ src/main/java/pages/HomePageClass.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePageClass {
+	//This is homepage classs
 	@FindBy(xpath="//*[@id='branding']//a[@id='welcome']")
 	private WebElement link;
 	
